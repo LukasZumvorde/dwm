2373ded25c57be491692714eb4b8a4da5ba97f22
Lukas Zumvorde <lukaszumvorde@web.de>
2020-10-19 17:54:08 +0200

    fix typo default.nix

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 stdenv.mkDerivation {
   name = "dwm-HEAD";
 
-  src = buildins.filterSource
+  src = builtins.filterSource
     (path: type: (toString path) != (toString ./.git)) ./.;
 
   buildInputs = [ libX11 libXft libXinerama ];
